22c6c8666ceaa2d6797e0cdea2cb25a457da952a
vain0x <vainzerox@gmail.com>
2020-09-30 00:54:13 +0900

    fix: typo

--- src/core.ts
+++ src/core.ts
@@ -6,7 +6,7 @@ export const PLACEHOLDER_REGEXP: RegExp = /:(\w+)/g
 /**
  * Since empty list `()` is illegal in SQL, use this sub-query that returns zero rows instead.
  *
- * Note that this doesn't work with legacy MySQL (< 5.6), where from clause is mandatory.
+ * Note that this doesn't work with legacy MySQL (<= 5.6), where from clause is mandatory.
  */
 export const EMPTY_LIST_SQL = "(select 0 where 0 = 1)"
 
