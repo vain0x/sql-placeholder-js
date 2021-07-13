7504df7062828622231597dcff7d177a00b7506c
vain0x <vainzerox@gmail.com>
2021-07-13 15:47:57 +0900

    refactor: Add tests on the regular expression

---
+++ src/core.test.ts
@@ -0,0 +1,34 @@
+import { deepStrictEqual as assertEq } from "assert"
+import { PLACEHOLDER_REGEXP } from "./core"
+
+declare const describe: (title: string, callback: () => void) => void
+declare const it: (title: string, callback: () => void) => void
+
+describe("PLACEHOLDER_REGEXP", () => {
+  const r = () => new RegExp(PLACEHOLDER_REGEXP)
+
+  it("matches basic identifier", () => {
+    const m = r().exec(":foo")
+    assertEq(m?.[1], "foo")
+  })
+
+  it("matches identifier with uppercase letters, digits and underscores", () => {
+    const m = r().exec(":Pascal_9_8")
+    assertEq(m?.[1], "Pascal_9_8")
+  })
+
+  it("doesn't match colon-only", () => {
+    const m = r().exec(": foo")
+    assertEq(m, null)
+  })
+
+  it("doesn't match non-ascii letter", () => {
+    const m = r().exec(":ゆ")
+    assertEq(m, null)
+  })
+
+  it("doesn't match emoji", () => {
+    const m = r().exec(":🐧")
+    assertEq(m, null)
+  })
+})
